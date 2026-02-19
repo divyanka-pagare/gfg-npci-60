e5f2e6edd01f845a0cf699ca18168249f89b4e98
Divyanka Pagare <113618065+divyanka-pagare@users.noreply.github.com>
2026-02-19 22:45:07 +0530

    Create Missing Element in Range.java

---
+++ Missing Element in Range.java
@@ -0,0 +1,19 @@
+class Solution {
+    public ArrayList<Integer> missingRange(int[] arr, int low, int high) {
+        // code here
+        ArrayList<Integer> result = new ArrayList<>();
+        
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            set.add(num);
+        }
+        
+        for (int i = low; i <= high; i++) {
+            if (!set.contains(i)) {
+                result.add(i);
+            }
+        }
+        
+        return result;
+    }
+}
