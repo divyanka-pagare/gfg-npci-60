ccc8acc0a9166bb15909202a0ca9bfeb4aa51196
Divyanka Pagare <113618065+divyanka-pagare@users.noreply.github.com>
2026-03-17 16:57:24 +0530

    Create Meeting Rooms.java

---
+++ Meeting Rooms.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    static boolean canAttend(int[][] arr) {
+        
+        Arrays.sort(arr, (a, b) -> a[0] - b[0]);
+        
+        
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i][0] < arr[i - 1][1]) {
+                return false; 
+            }
+        }
+        
+        return true;
+    }
+}
